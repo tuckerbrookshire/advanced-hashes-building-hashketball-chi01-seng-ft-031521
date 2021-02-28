b192cb7c7286c7c86ec2f739132518b78eadc6bb
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-28 20:12:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -47,7 +47,7 @@ def game_hash
             steals: 3, 
             blocks: 8, 
             slam_dunks: 5,
-            }
+            },
             {
             player_name: "Jason Terry",
             number: 31,
@@ -58,7 +58,7 @@ def game_hash
             steals: 4,
             blocks: 11,
             slam_dunks: 1
-            },
+            }
           ]
       }
     away: 
