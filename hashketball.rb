ecc3e016f4536d429bba793805156968b9eb1209
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-28 19:47:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,8 +1,34 @@
 def game_hash
    { 
+     home: 
+      { team_name: "Brooklyn Nets", 
+        colors:[""], 
+       players: [
+            {player_name:}
+            {number:}
+            {shoe:}
+            {points:}
+            {rebounds:}
+            {assists:}
+            {steals:}
+            {blocks:}
+            {slam_dunks:}
+          ]
+      }
     away: 
-      {team_name: "Charlotte Hornets"}
-    home: 
-      {team_name: "Brooklyn Nets"}
+      { team_name: "Charlotte Hornets", 
+        colors:[""], 
+        players: [
+            {player_name:}
+            {number:}
+            {shoe:}
+            {points:}
+            {rebounds:}
+            {assists:}
+            {steals:}
+            {blocks:}
+            {slam_dunks:}
+          ]
+      }
    }
 end
