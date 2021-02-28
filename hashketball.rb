e5448923b09a6d9e090273710898e83fe446d625
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-28 19:55:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,14 +6,14 @@ def game_hash
         players: [
           {
             player_name: "Alan Anderson",
-            number:
-            shoe:
-            points:
-            rebounds:
-            assists:
-            steals:
-            blocks:
-            slam_dunks:
+            number: 0,
+            shoe: 16,
+            points: 22, 
+            rebounds: 12,
+            assists: 12,
+            steals: 3,
+            blocks: 1,
+            slam_dunks: 1
           },
           {
             player_name: "",
